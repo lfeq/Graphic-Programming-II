1fe00120c84d03f27291d1da85ca152501d39ffa
lfeq <panchocosa@gmail.com>
2024-01-30 16:44:29 -0600

    Rubiks cube finished

--- Rubik.h
+++ Rubik.h
@@ -4,12 +4,12 @@
 
 class Rubik {
 public:
-	Cube row1[3][3];
-	Cube row2[3][3];
-	Cube row3[3][3];
-	Cube column1[3][3];
-	Cube column2[3][3];
-	Cube column3[3][3];
+	Cube* row1[3][3];
+	Cube* row2[3][3];
+	Cube* row3[3][3];
+	Cube* column1[3][3];
+	Cube* column2[3][3];
+	Cube* column3[3][3];
 	Rubik();
 	void RotateRow1();
 	void RotateRow2();
@@ -22,15 +22,15 @@ public:
 
 Rubik::Rubik() {
 	// Row 1
-	Cube one_one(0, 0, -15);
-	Cube one_two(3, 0, -15);
-	Cube one_three(6, 0, -15);
-	Cube two_one(0, 0, -18);
-	Cube two_two(3, 0, -18);
-	Cube two_three(6, 0, -18);
-	Cube three_one(0, 0, -21);
-	Cube three_two(3, 0, -21);
-	Cube three_three(6, 0, -21);
+	Cube* one_one = new Cube(0, 0, -15);
+	Cube* one_two = new Cube(3, 0, -15);
+	Cube* one_three = new Cube(6, 0, -15);
+	Cube* two_one = new Cube(0, 0, -18);
+	Cube* two_two = new Cube(3, 0, -18);
+	Cube* two_three = new Cube(6, 0, -18);
+	Cube* three_one = new Cube(0, 0, -21);
+	Cube* three_two = new Cube(3, 0, -21);
+	Cube* three_three = new Cube(6, 0, -21);
 	row1[0][0] = one_one;
 	row1[0][1] = one_two;
 	row1[0][2] = one_three;
@@ -42,15 +42,15 @@ Rubik::Rubik() {
 	row1[2][2] = three_three;
 
 	// Row 2
-	Cube four_one(0, 3, -15);
-	Cube four_two(3, 3, -15);
-	Cube four_three(6, 3, -15);
-	Cube five_one(0, 3, -18);
-	Cube five_two(3, 3, -18);
-	Cube five_three(6, 3, -18);
-	Cube six_one(0, 3, -21);
-	Cube six_two(3, 3, -21);
-	Cube six_three(6, 3, -21);
+	Cube* four_one = new Cube(0, 3, -15);
+	Cube* four_two = new Cube(3, 3, -15);
+	Cube* four_three = new Cube(6, 3, -15);
+	Cube* five_one = new Cube(0, 3, -18);
+	Cube* five_two = new Cube(3, 3, -18);
+	Cube* five_three = new Cube(6, 3, -18);
+	Cube* six_one = new Cube(0, 3, -21);
+	Cube* six_two = new Cube(3, 3, -21);
+	Cube* six_three = new Cube(6, 3, -21);
 	row2[0][0] = four_one;
 	row2[0][1] = four_two;
 	row2[0][2] = four_three;
@@ -62,15 +62,15 @@ Rubik::Rubik() {
 	row2[2][2] = six_three;
 
 	// Row 3
-	Cube seven_one(0, 6, -15);
-	Cube seven_two(3, 6, -15);
-	Cube seven_three(6, 6, -15);
-	Cube eight_one(0, 6, -18);
-	Cube eight_two(3, 6, -18);
-	Cube eight_three(6, 6, -18);
-	Cube nine_one(0, 6, -21);
-	Cube nine_two(3, 6, -21);
-	Cube nine_three(6, 6, -21);
+	Cube* seven_one = new Cube(0, 6, -15);
+	Cube* seven_two = new Cube(3, 6, -15);
+	Cube* seven_three = new Cube(6, 6, -15);
+	Cube* eight_one = new Cube(0, 6, -18);
+	Cube* eight_two = new Cube(3, 6, -18);
+	Cube* eight_three = new Cube(6, 6, -18);
+	Cube* nine_one = new Cube(0, 6, -21);
+	Cube* nine_two = new Cube(3, 6, -21);
+	Cube* nine_three = new Cube(6, 6, -21);
 	row3[0][0] = seven_one;
 	row3[0][1] = seven_two;
 	row3[0][2] = seven_three;
@@ -113,31 +113,27 @@ Rubik::Rubik() {
 	column3[2][0] = seven_three;
 	column3[2][1] = eight_three;
 	column3[2][2] = nine_three;
-
-	std::cout << "Cube uno: " << &one_one << std::endl;
-	std::cout << "Cube uno columna 1: " << &column1[0][0] << std::endl;
-	std::cout << "Cube uno fila 1: " << &row1[0][0] << std::endl;
 }
 
 void Rubik::Draw() {
 	for (int x = 0; x < 3; x++) {
 		for (int y = 0; y < 3; y++) {
 			glPushMatrix();
-			row1[x][y].Draw();
+			row1[x][y]->Draw();
 			glPopMatrix();
 		}
 	}
 	for (int x = 0; x < 3; x++) {
 		for (int y = 0; y < 3; y++) {
 			glPushMatrix();
-			row2[x][y].Draw();
+			row2[x][y]->Draw();
 			glPopMatrix();
 		}
 	}
 	for (int x = 0; x < 3; x++) {
 		for (int y = 0; y < 3; y++) {
 			glPushMatrix();
-			row3[x][y].Draw();
+			row3[x][y]->Draw();
 			glPopMatrix();
 		}
 	}
@@ -148,7 +144,7 @@ void Rubik::RotateRow1() {
 		for (int y = 0; y < 3; y++) {
 			glPushMatrix();
 			//row1[x][y].Rotate(5, 1, 0, 0);
-			row1[x][y].RotateY(90.0f);
+			row1[x][y]->RotateY(90.0f);
 			glPopMatrix();
 		}
 	}
@@ -159,7 +155,7 @@ void Rubik::RotateRow2() {
 		for (int y = 0; y < 3; y++) {
 			glPushMatrix();
 			//row2[x][y].Rotate(45, 1, 0, 0);
-			row2[x][y].RotateY(90.0f);
+			row2[x][y]->RotateY(90.0f);
 			glPopMatrix();
 		}
 	}
@@ -170,20 +166,18 @@ void Rubik::RotateRow3() {
 		for (int y = 0; y < 3; y++) {
 			glPushMatrix();
 			//row3[x][y].Rotate(45, 1, 0, 0);
-			row3[x][y].RotateY(90.0f);
+			row3[x][y]->RotateY(90.0f);
 			glPopMatrix();
 		}
 	}
 }
 
 void Rubik::RotateColumn1() {
-	std::cout << "Cube uno columna 1: " << &column1[0][0] << std::endl;
-	std::cout << "Cube uno fila 1: " << &row1[0][0] << std::endl;
 	for (int x = 0; x < 3; x++) {
 		for (int y = 0; y < 3; y++) {
 			glPushMatrix();
 			//row1[x][y].Draw();
-			column1[x][y].RotateX(90.0f);
+			column1[x][y]->RotateX(90.0f);
 			glPopMatrix();
 		}
 	}
@@ -195,7 +189,7 @@ void Rubik::RotateColumn2() {
 			glPushMatrix();
 			//row1[x][y].Draw();
 			//column2[x][y].Rotate(45, 1, 0, 0);
-			column2[x][y].RotateX(90.0f);
+			column2[x][y]->RotateX(90.0f);
 			glPopMatrix();
 		}
 	}
@@ -206,7 +200,7 @@ void Rubik::RotateColumn3() {
 		for (int y = 0; y < 3; y++) {
 			glPushMatrix();
 			//row1[x][y].Draw();
-			column3[x][y].RotateX(90.0f);
+			column3[x][y]->RotateX(90.0f);
 			glPopMatrix();
 		}
 	}
